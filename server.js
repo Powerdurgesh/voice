206d3bd67123a6452f1660287651d0423886ebb0
Powerdurgesh <chintalapowerdurgesh2003@gmail.com>
2025-07-13 23:50:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -99,9 +99,10 @@ try {
     res.redirect('/client.html');
   });
 
-  server.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
-  });
+  const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 
 } catch (err) {
   console.error('💥 Server crashed:', err);
